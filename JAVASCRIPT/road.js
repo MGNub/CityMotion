111b703d5db6fd50291df3ae1b03b570d805127e
Youxi Pan <130309628+MGNub@users.noreply.github.com>
2024-04-22 02:38:52 -0400

    Update road.js

--- JAVASCRIPT/road.js
+++ JAVASCRIPT/road.js
@@ -29,7 +29,7 @@ for(let i = 0; i < text.length; i++) {
     }
 }
 
-//numberAnimate
+numberAnimate
 
 function numberLoad() {
     let numbers = '1234567890';
@@ -54,47 +54,54 @@ function numberLoad() {
 }
 
 function parallax() {
+    const pageHeight = window.innerHeight;
+
     const pageOneText = document.getElementById('pageText1');
     const pages = document.getElementById('pages');
     const roadP = document.getElementById('roadP');
     const roadLine = document.getElementById('roadLine');
     const streetP = document.getElementById('streetP');
     const streetLine = document.getElementById('streetLine');
-        pageOneText.style.top = -0.33 * window.pageYOffset + window.innerHeight / 2;
-        pageOneText.style.opacity = 1 - window.pageYOffset / 500;
+    const imageText = document.getElementById('imageText');
+
+        // pageOneText.style.top = -0.33 * window.pageYOffset + pageHeight / 2;
+        // pageOneText.style.opacity = 1 - window.pageYOffset / 500;
 
         // pages.style.top = -1 * window.pageYOffset;
-        
-        if(window.scrollY > window.innerHeight * 7) {
+        if (window.scrollY > pageHeight * 14.4) {
+            imageText.style.marginLeft = pageHeight * -3.7 + window.innerWidth / 2;
+        } else if (window.scrollY > pageHeight * 10.7) {
+            imageText.style.marginLeft = pageHeight * 10.7 - window.pageYOffset + window.innerWidth / 2;
+        } else if(window.scrollY > pageHeight * 7) {
             streetP.innerText = 'Since streets have more human activity, it has more safety features to accomodate for pedestrians. An ideal street is lively, nice to look at, and provides value to the city.';
             streetP.style.opacity = '1';
             streetP.style.transform = 'translateY(0%)';
-        } else if(window.scrollY > window.innerHeight * 7 - 300) {
+        } else if(window.scrollY > pageHeight * 7 - 300) {
             streetP.style.opacity = '0';
             streetP.style.transform = 'translateY(-20%)';
-        } else if(window.scrollY > window.innerHeight * 6) {
+        } else if(window.scrollY > pageHeight * 6) {
             streetP.innerText = 'Streets are more narrow than roads, and they are lined up with buildings on the sides with wider sidewalks. Streets promotes wealth for the city and encourages economic growth.';
             streetP.style.opacity = '1';
             streetP.style.transform = 'translateY(0%)';
-        } else if(window.scrollY > window.innerHeight * 6 - 300) {
+        } else if(window.scrollY > pageHeight * 6 - 300) {
             streetP.style.opacity = '0';
             streetP.style.transform = 'translateY(-20%)';
-        } else if(window.scrollY > window.innerHeight * 5) {
+        } else if(window.scrollY > pageHeight * 5) {
             streetP.innerText = 'While roads serve as routes or pathways between destinations with a greater distance, streets serve as local routes for vehicles and pedestrians.';
             streetP.style.opacity = '1';
             streetP.style.transform = 'translateY(0%)';
-        } else if (window.scrollY > window.innerHeight * 3.5) {
+        } else if (window.scrollY > pageHeight * 3.5) {
             roadP.innerText = "A well-designed road is safe for all users, including drivers, motorists, and bikers. A good road is fast and reliable, while badly designed road is dangerous or cause traffic congestion.";
             roadP.style.opacity = '1';
             roadP.style.transform = 'translateY(0%)';
-        } else if (window.scrollY > window.innerHeight * 3.5 - 300) {
+        } else if (window.scrollY > pageHeight * 3.5 - 300) {
             roadP.style.opacity = '0';
             roadP.style.transform = 'translateY(-20%)';
-        } else if (window.scrollY > window.innerHeight * 2.5) {
+        } else if (window.scrollY > pageHeight * 2.5) {
             roadP.innerText = 'Generally, roads are designed for higher speeds. They usually pass through rural or less densely populated areas, with fewer structures directly adjacent to the roadway.';
             roadP.style.opacity = '1';
             roadP.style.transform = 'translateY(0%)';
-        } else if (window.scrollY > window.innerHeight * 2.5 - 300) {
+        } else if (window.scrollY > pageHeight * 2.5 - 300) {
             roadP.style.opacity = '0';
         } else if (window.scrollY > 0) {
             roadP.innerText = 'The purpose of roads is to transport people and goods fast and efficiently. Roads are high-speed thoroughfares, which are connections between 2 places.';
@@ -105,12 +112,32 @@ function parallax() {
         }
 
         if (window.scrollY > 0) {
-            roadLine.style.width = -window.innerHeight / 3 + window.pageYOffset / 4.5;
-            streetLine.style.width = -window.innerHeight - 100 + window.pageYOffset / 4.5;
+            roadLine.style.width = -pageHeight / 3 + window.pageYOffset / 4.5;
+            streetLine.style.width = -pageHeight - 100 + window.pageYOffset / 4.5;
         }
-        // if (window.pageYOffset > window.innerHeight * 1.75) {
+        // if (window.pageYOffset > pageHeight * 1.75) {
         //     document.getElementById('pageText3').classList.add('show');
         // }
+
+        /*parallaxText*/
+        const parallaxTexts = document.getElementsByClassName('parallaxText');
+        // const parallaxShapes = document.getElementsByClassName('parallaxShape');
+        const page6 = document.getElementById('page6');
+
+        if (window.scrollY > pageHeight * 14.4) {
+            page6.style.background = 'var(--primaryDark)';
+        } else {
+            page6.style.background = 'black';
+        }
+
+        parallaxTexts[0].style.top = (pageHeight * 15.9 - window.pageYOffset) / 1.5;
+        parallaxTexts[1].style.top = (pageHeight * 17 - window.pageYOffset) / 0.8;
+        parallaxTexts[2].style.top = (pageHeight * 17.3 - window.pageYOffset) / 2;
+        parallaxTexts[3].style.top = (pageHeight * 17.6 - window.pageYOffset) / 0.5; 
+
+        // parallaxShapes[0].style.top = (pageHeight * 18 - window.pageYOffset) / 2
+        // parallaxShapes[1].style.top = (pageHeight * 17.5 - window.pageYOffset) / 3;
+
 
 }
 
