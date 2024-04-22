279647a3018146bd01bb8830b1fe4b2030ec0035
Youxi Pan <130309628+MGNub@users.noreply.github.com>
2024-04-22 02:23:47 -0400

    Update home.js

--- JAVASCRIPT/home.js
+++ JAVASCRIPT/home.js
@@ -21,21 +21,21 @@ function parallax() {
         logo.classList.remove('rotate');
     }
 
-    if (scrollY < 730 * 3.2) {
-        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (window.pageYOffset / 10 - 730 / 5) + 'px)';
+    if (scrollY < pageHeight * 3.2) {
+        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (window.pageYOffset / 15.4 - pageHeight / 5.25) + 'vh)';
     }
-    else if (scrollY < 730 * 4) {
-        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (730 * 3.2 / 10 - 730 / 5) + 'px)';
-        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((window.pageYOffset / 22.5) - 730 / 7.5) + 'px)';
+    else if (scrollY < pageHeight * 4.2) {
+        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (pageHeight * 3.2 / 15.4 - pageHeight / 5.25) + 'vh)';
+        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((window.pageYOffset / 22.5) - pageHeight / 7) + 'px)';
         card2.style.opacity = -8 + window.pageYOffset / 320;
-        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((window.pageYOffset / 22.5) - 730 / 7.5) + 'px)';
+        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((window.pageYOffset / 22.5) - pageHeight / 7) + 'px)';
         text2.style.opacity = -8 + window.pageYOffset / 320;
     } else {
-        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (730 * 3.2 / 10 - 730 / 5) + 'px)'; //try converting px to % or smthin.
-        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((730 * 4 / 22.5) - 730 / 7.5) + 'px)';
-        card2.style.opacity = -8 + 730 * 4 / 320;
-        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((730 * 4 / 22.5) - 730 / 7.5) + 'px)';
-        text2.style.opacity = -8 + 730 * 4 / 320;
+        card1.style.transform = 'translateX(-35%) rotateY(-1deg) translateZ(' + (pageHeight * 3.2 / 15.4 - pageHeight / 5.25) + 'vh)'; //try converting px to % or smthin.
+        card2.style.transform = 'translateX(30%) rotateY(1deg) translateZ(' + ((pageHeight * 4.2 / 22.5) - pageHeight / 7) + 'px)';
+        card2.style.opacity = -8 + pageHeight * 4.2 / 320;
+        text2.style.transform = 'translateX(-50%) rotateY(-2deg) translateZ(' + ((pageHeight * 4.2 / 22.5) - pageHeight / 7) + 'px)';
+        text2.style.opacity = -8 + pageHeight * 4.2 / 320;
     }
 }
 
@@ -71,45 +71,3 @@ routeBtn.addEventListener('mousedown', function() {
 routeBtn.addEventListener('mouseout', function() {
     innerBtn.classList.remove('btnClicked');
 })
-
-
-//function parallax() {
-//     const horizontal = document.getElementById('horizontal');
-//     const vertical = document.getElementById('vertical');
-//     const videoBG = document.getElementById('background');
-//     const video = document.getElementById('backgroundVideo');
-//     const postVideoCard = document.getElementById('postVideo');
-//     const overlay = document.getElementById('overlay');
-//     const introBox = document.getElementById('introBox');
-    
-    
-//     if(window.pageYOffset < window.innerWidth * 2.5) {
-//         horizontal.style.left = -1.2 * window.pageYOffset;
-//         videoBG.style.left = -1 * window.pageYOffset / 5;
-//         overlay.style.left = -1 * window.pageYOffset / 5;
-//         videoBG.style.top = 0;
-//         postVideoCard.style.marginLeft = window.innerWidth * 2.5 - window.pageYOffset;
-//     } else if(window.pageYOffset > window.innerWidth * 2) {
-//         videoBG.style.left = window.innerWidth * -2.5/5;
-//         horizontal.style.left = window.innerWidth * -2.5 * 1.2;
-//         videoBG.style.top = window.pageYOffset - window.innerWidth * 2.5;
-//         postVideoCard.style.marginLeft = window.innerWidth * 2 - window.pageYOffset * 1.75;
-//     }
-
-//     if(window.scrollY > window.innerWidth / 1.2) {
-//         video.style.marginTop = -window.innerWidth / 8;
-//         video.style.width = window.innerWidth * 1.7;
-//         titleText.classList.add('fade');
-//         logo.classList.add('rotate');
-//         introBox.style.left = -1 * window.innerWidth;
-//     } else {
-//         video.style.marginTop = 0;
-//         video.style.width = window.innerWidth;
-//         titleText.classList.remove('fade');
-//         logo.classList.remove('rotate');
-//         introBox.style.left = window.innerWidth / 2;
-//     }
-
-// }
-
-// window.addEventListener('scroll', parallax);
